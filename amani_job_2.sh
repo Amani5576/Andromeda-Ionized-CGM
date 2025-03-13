61446af99c31d779776625bc6a6d020c52da9f54
Amani5576 <smmnjoroge@gmail.com>
2025-03-13 17:30:28 +0200

    adjusting job script to reflect correct stat info

--- amani_job_2.sh
+++ amani_job_2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name="m31_Individual_plots_on_RM_vs_rad_proj"
-#SBATCH --output=log_stash/m31-Individual-plots-on-RM-vs-rad-proj-%j-%Y%m%d-%H%M%S.log
-#SBATCH --error=log_stash/m31-Individual-plots-on-RM-vs-rad-proj-%j-%Y%m%d-%H%M%S-error.log
+#SBATCH --output=log_stash/m31-Individual-plots-on-RM-vs-rad-proj-%j.log
+#SBATCH --error=log_stash/m31-Individual-plots-on-RM-vs-rad-proj-%j-error.log
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=20G
@@ -11,15 +11,15 @@
 # Load required modules
 echo "Submitting Slurm Job for Testing individual plots of Honours |RM| against radial projection"
 
-# Running main.py first to define variables
-singularity exec /idia/software/containers/ASTRO-PY3.simg python3 main.py
-
 #Script of interest to be run in current job
 singularity exec /idia/software/containers/ASTRO-PY3.simg python3 M31_signal_vs_entire_sky.py 
 
+# Wait for job to complete before logging stats
+wait
 
 # Capture job statistics after completion
-echo "----------------------------------------------------" >> m31-Individual-plots-on-RM-vs-rad-proj-$SLURM_JOB_ID.log
-echo "SLURM Job Efficiency Report for Job ID: $SLURM_JOB_ID" >> m31-Individual-plots-on-RM-vs-rad-proj-$SLURM_JOB_ID.log
-seff $SLURM_JOB_ID >> m31-Individual-plots-on-RM-vs-rad-proj-$SLURM_JOB_ID.log
-echo "----------------------------------------------------" >> m31-Individual-plots-on-RM-vs-rad-proj-$SLURM_JOB_ID.log
+LOG_FILE="log_stash/m31-Individual-plots-on-RM-vs-rad-proj-$SLURM_JOB_ID.log"
+echo "----------------------------------------------------" >> $LOG_FILE
+echo "SLURM Job Efficiency Report for Job ID: $SLURM_JOB_ID" >> $LOG_FILE
+sacct -j $SLURM_JOB_ID --format=JobID,MaxRSS,Elapsed,State >> $LOG_FILE
+echo "----------------------------------------------------" >> $LOG_FILE
