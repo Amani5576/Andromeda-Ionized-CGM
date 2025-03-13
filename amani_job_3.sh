dcdec979cab39a338eef2990adeb0b07f0442bd9
Amani5576 <smmnjoroge@gmail.com>
2025-03-13 21:20:32 +0200

    correcting information in bash files

--- amani_job_3.sh
+++ amani_job_3.sh
@@ -3,13 +3,13 @@
 #SBATCH --output=log_stash/m31-Individual-plots-on-RM-vs-rad-proj-%j.log
 #SBATCH --error=log_stash/m31-Individual-plots-on-RM-vs-rad-proj-%j-error.log
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=10
-#SBATCH --mem=35G
-#SBATCH --time=00:40:00
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=16G
+#SBATCH --time=00:18:00
 #SBATCH --partition=Main
 
 # Load required modules
-echo "Submitting Slurm Job for Testing individual plots of Honours |RM| against radial projection"
+echo "Submitting Slurm Job for plotting Histograms after presenting stacked (non-mean) verison at 12th March Wednesday meeting"
 
 #Script of interest to be run in current job
-singularity exec /idia/software/containers/ASTRO-PY3.simg python3 M31_signal_vs_entire_sky.py
+singularity exec /idia/software/containers/ASTRO-PY3.simg python3 M31_signal_vs_entire_sky.py --annuli-anal
