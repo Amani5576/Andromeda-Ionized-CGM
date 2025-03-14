7e45b3a6a5e4c6810c08834aeae28008426c08d5
Amani5576 <smmnjoroge@gmail.com>
2025-03-14 15:14:11 +0200

    adjusting cpu and memory required

--- amani_job_3.sh
+++ amani_job_3.sh
@@ -3,8 +3,8 @@
 #SBATCH --output=log_stash/m31_histogram_annuli_analysis-%j.log
 #SBATCH --error=log_stash/m31_histogram_annuli_analysis-%j-error.log
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=6
-#SBATCH --mem=20G
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=25G
 #SBATCH --time=00:30:00
 #SBATCH --partition=Main
 
