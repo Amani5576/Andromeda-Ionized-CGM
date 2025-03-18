69381336c20fb05b7145c9e551b70d593da8d0a0
Amani5576 <smmnjoroge@gmail.com>
2025-03-18 13:35:05 +0200

    adding more description

--- amani_job_4.sh
+++ amani_job_4.sh
@@ -9,7 +9,7 @@
 #SBATCH --partition=Main
 
 # Load required modules
-echo "Submitting Slurm Job for plotting Histograms after presenting stacked (non-mean) verison at 12th March Wednesday meeting"
+echo "Submitting Slurm Job for plotting Histograms of random patches of the sky, but with a super-imposing of all histograms"
 
 #Script of interest to be run in current job
 singularity exec /idia/software/containers/ASTRO-PY3.10-2024-10-18.sif python3 M31_signal_vs_entire_sky.py --annuli-anal --overplot
