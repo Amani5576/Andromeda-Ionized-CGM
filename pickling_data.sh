239937452cfa8a3ab9233cf3cfc3dd8eb446450e
Amani5576 <smmnjoroge@gmail.com>
2025-04-01 00:05:32 +0200

    increasing even more RAM and CPU

--- pickling_data.sh
+++ pickling_data.sh
@@ -3,9 +3,9 @@
 #SBATCH --output=log_stash/Overwriting-pickled-data-BG-Subtraction-Spline-%j.log
 #SBATCH --error=log_stash/Overwriting-pickled-data-BG-Subtraction-Spline-%j-error.log
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=6
-#SBATCH --mem=50G
-#SBATCH --time=04:00:00
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=70G
+#SBATCH --time=08:00:00
 #SBATCH --partition=Main
 
 # Load required modules
