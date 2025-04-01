26121909f8b6d53c5b5a090eaca811c5632f2804
Amani5576 <smmnjoroge@gmail.com>
2025-04-01 14:22:34 +0200

    adding new bash script for Original plot after correct and working RM subtraction

---
+++ pickle_and_orginal_plot.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name="Pickle-and-Original-Plot-BG-Subtraction%j"
+#SBATCH --output=log_stash/Pickle-and-Original-Plot-BG-Subtraction-%j.log
+#SBATCH --error=log_stash/Pickle-and-Original-Plot-BG-Subtraction-%j-error.log
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=20
+#SBATCH --mem=100G
+#SBATCH --time=07:00:00
+#SBATCH --partition=Main
+
+echo "Submitting Slurm Job: BG Subtraction with Spline, Pickling, and Plotting."
+
+# Run the Python script for BG subtraction, saving plots, and pickling data
+singularity exec /idia/software/containers/ASTRO-PY3.10-2024-10-18.sif python3 M31_signal_vs_entire_sky.py --original-plot --save-plot --pickling
+
+echo "Job completed: Pickling and Original Plot generation finished."
--- pickling_data.sh
+++ pickling_data.sh
@@ -3,13 +3,13 @@
 #SBATCH --output=log_stash/Overwriting-pickled-data-BG-Subtraction-Spline-%j.log
 #SBATCH --error=log_stash/Overwriting-pickled-data-BG-Subtraction-Spline-%j-error.log
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=20
-#SBATCH --mem=100G
-#SBATCH --time=07:00:00
+#SBATCH --cpus-per-task=18
+#SBATCH --mem=80G
+#SBATCH --time=05:00:00
 #SBATCH --partition=Main
 
 # Load required modules
 echo "Submitting Slurm Job: Proper BG subtraction with spline and pickling."
 
 # Run the Python script for BG subtraction with pickling enabled
-singularity exec /idia/software/containers/ASTRO-PY3.10-2024-10-18.sif python3 M31_signal_vs_entire_sky.py --original-plot --save-plot --pickling
+singularity exec /idia/software/containers/ASTRO-PY3.10-2024-10-18.sif python3 M31_signal_vs_entire_sky.py --pickling
